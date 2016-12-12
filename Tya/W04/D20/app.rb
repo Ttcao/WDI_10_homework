f5110b296bc4393240f847a7c218f88933107f19
Tya Cao <cao.tya@gmail.com>
2016-12-12 22:41:36 +1100

    Update app.rb file

--- Tya/W04/D20/app.rb
+++ Tya/W04/D20/app.rb
@@ -3,21 +3,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
 
-# Movies I
-# Specification
-
-# Build a search form that lets the user enter a movie name. The Sinatra app will use HTTParty to fetch the data for the chosen movie from OMDB and display it on the page.
-
-# display movie searches
 get '/' do
   @movie_title = params[:movie]
   @result = HTTParty.get("http://omdbapi.com/?s=#{@movie_title}")
+  puts @result["Search"][5]
   binding.pry
   erb :index
+
 end
 
+  # result = HTTParty.get('http://omdbapi.com/?t=once')
+
 # look through array and deal with each hash
-# how to iterate through each 
+# how to iterate through each
 # search results
 # each 'search results' hash has arrays of movie hashes, with elements inside which are hashes
 
