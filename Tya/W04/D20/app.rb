1597028b06f7bf88c6ebd633fa17ab708f84df4a
Tya Cao <cao.tya@gmail.com>
2016-12-12 19:24:47 +1100

    Create files and connect to Omdb API.

--- Tya/W04/D20/app.rb
+++ Tya/W04/D20/app.rb
@@ -8,8 +8,20 @@ require 'httparty'
 
 # Build a search form that lets the user enter a movie name. The Sinatra app will use HTTParty to fetch the data for the chosen movie from OMDB and display it on the page.
 
+# display movie searches
 get '/' do
   @movie_title = params[:movie]
-  @result = HTTParty.get('http://omdbapi.com/?t=@movie_title')
+  @result = HTTParty.get("http://omdbapi.com/?s=#{@movie_title}")
+  binding.pry
   erb :index
 end
+
+# look through array and deal with each hash
+# how to iterate through each 
+# search results
+# each 'search results' hash has arrays of movie hashes, with elements inside which are hashes
+
+# display movie titles
+# get '/about' do
+#   erb :about
+# end
